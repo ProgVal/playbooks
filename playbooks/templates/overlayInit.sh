93bee872c18efe8201a3c9805621a733c884da5b
Valentin Lorentz <progval+ansible@progval.net>
2018-12-04 22:33:42 +0100

    Overlay of /var too

--- playbooks/templates/overlayInit.sh
+++ playbooks/templates/overlayInit.sh
@@ -13,6 +13,17 @@ mkdir /mnt/__upper_etc/workdir
 echo "overlaying /etc"
 mount -t overlay overlay -o lowerdir=/etc,upperdir=/mnt/__upper_etc/data,workdir=/mnt/__upper_etc/workdir /etc
 
+echo "mounting /mnt/__upper_var"
+mkdir -p /mnt/__upper_var
+mount -t tmpfs tmpfs /mnt/__upper_var
+
+echo "making directories in /mnt/__upper_var"
+mkdir /mnt/__upper_var/data
+mkdir /mnt/__upper_var/workdir
+
+echo "overlaying /var"
+mount -t overlay overlay -o lowerdir=/var,upperdir=/mnt/__upper_var/data,workdir=/mnt/__upper_var/workdir /var
+
 echo "executing init in 5 seconds"
 sleep 5
 exec /sbin/init
