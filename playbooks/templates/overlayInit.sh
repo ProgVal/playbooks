2648e679c1e58e7c1a066baa103b9ec548b28896
Valentin Lorentz <progval+ansible@progval.net>
2018-12-04 22:30:13 +0100

    Fully read-only boot + prepare for overlayed root

---
+++ playbooks/templates/overlayInit.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo "loading overlay kernel module"
+modprobe overlay
+
+echo "mounting /mnt/__upper_etc"
+mkdir -p /mnt/__upper_etc
+mount -t tmpfs tmpfs /mnt/__upper_etc
+
+echo "making directories in /mnt/__upper_etc"
+mkdir /mnt/__upper_etc/data
+mkdir /mnt/__upper_etc/workdir
+
+echo "overlaying /etc"
+mount -t overlay overlay -o lowerdir=/etc,upperdir=/mnt/__upper_etc/data,workdir=/mnt/__upper_etc/workdir /etc
+
+echo "executing init in 5 seconds"
+sleep 5
+exec /sbin/init
