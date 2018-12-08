747f13f9a5dbd0af70f6ef35a3f5f4e9a019cd0d
Valentin Lorentz <progval+ansible@progval.net>
2018-12-08 16:37:58 +0100

    crypt etc

--- playbooks/templates/overlayInit.sh
+++ playbooks/templates/overlayInit.sh
@@ -2,6 +2,14 @@
 echo "loading overlay kernel module"
 modprobe overlay
 
+echo "decrypting middle etc"
+mount /dev/mapper/
+
+echo "mounting /mnt/__middle_etc"
+mkdir -p /mnt/__middle_etc
+cryptsetup luksOpen UUID={{etc_uuid}} {{etc_crypt_target}}
+mount /dev/mapper/{{etc_crypt_target}} /mnt/__middle_etc
+
 echo "mounting /mnt/__upper_etc"
 mkdir -p /mnt/__upper_etc
 mount -t tmpfs tmpfs /mnt/__upper_etc
@@ -11,7 +19,7 @@ mkdir /mnt/__upper_etc/data
 mkdir /mnt/__upper_etc/workdir
 
 echo "overlaying /etc"
-mount -t overlay overlay -o lowerdir=/etc,upperdir=/mnt/__upper_etc/data,workdir=/mnt/__upper_etc/workdir /etc
+mount -t overlay overlay -o lowerdir=/etc:/mnt/__middle_etc,upperdir=/mnt/__upper_etc/data,workdir=/mnt/__upper_etc/workdir /etc
 
 echo "executing init now"
 exec /sbin/init
