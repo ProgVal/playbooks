ff7185316c499026ed33383c564c81493f511f1c
Valentin Lorentz <progval+ansible@progval.net>
2018-12-09 01:07:20 +0100

    working full etc overlay.

--- playbooks/templates/overlayInit.sh
+++ playbooks/templates/overlayInit.sh
@@ -2,24 +2,23 @@
 echo "loading overlay kernel module"
 modprobe overlay
 
-echo "decrypting middle etc"
-mount /dev/mapper/
-
 echo "mounting /mnt/__middle_etc"
 mkdir -p /mnt/__middle_etc
-cryptsetup luksOpen UUID={{etc_uuid}} {{etc_crypt_target}}
-mount /dev/mapper/{{etc_crypt_target}} /mnt/__middle_etc
+#cat /dev/vdb | cryptsetup luksOpen UUID={{etc_uuid}} {{etc_crypt_target}}
+#mount /dev/mapper/{{etc_crypt_target}} /mnt/__middle_etc
+cat /dev/vdb | cut -d '' -f1 | tr -d '\n' > /run/etc_keyfile # 'cut' replaces trailing \x00 bytes with a newline, tr removes that newline.
+chmod u=r,g=,o= /run/etc_keyfile
 
 echo "mounting /mnt/__upper_etc"
 mkdir -p /mnt/__upper_etc
 mount -t tmpfs tmpfs /mnt/__upper_etc
 
 echo "making directories in /mnt/__upper_etc"
-mkdir /mnt/__upper_etc/data
+mkdir /mnt/__upper_etc/datadir
 mkdir /mnt/__upper_etc/workdir
 
 echo "overlaying /etc"
-mount -t overlay overlay -o lowerdir=/etc:/mnt/__middle_etc,upperdir=/mnt/__upper_etc/data,workdir=/mnt/__upper_etc/workdir /etc
+#mount -t overlay overlay -o lowerdir=/etc:/mnt/__middle_etc,upperdir=/mnt/__upper_etc/data,workdir=/mnt/__upper_etc/workdir /etc
 
 echo "executing init now"
 exec /sbin/init
