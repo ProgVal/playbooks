59b911cde199bc0197496898e852742991ff2416
Valentin Lorentz <progval+ansible@progval.net>
2018-12-11 23:07:58 +0100

    load the keyfile from a systemd unit + fix dependencies.

--- playbooks/templates/overlayInit.sh
+++ playbooks/templates/overlayInit.sh
@@ -2,12 +2,5 @@
 echo "loading overlay kernel module"
 modprobe overlay
 
-echo "preparing /mnt/__middle_etc"
-mkdir -p /mnt/__middle_etc
-#cat /dev/vdb | cryptsetup luksOpen UUID={{etc_uuid}} {{etc_crypt_target}}
-#mount /dev/mapper/{{etc_crypt_target}} /mnt/__middle_etc
-cat /dev/vdb | cut -d '' -f1 | tr -d '\n' > /run/etc_keyfile # 'cut' replaces trailing \x00 bytes with a newline, tr removes that newline.
-chmod u=r,g=,o= /run/etc_keyfile
-
 echo "executing init now"
 exec /sbin/init
