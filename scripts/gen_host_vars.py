23b0614e8a89d38cb8c90316da638e34d6f56c3e
Valentin Lorentz <progval+ansible@progval.net>
2018-12-15 19:39:53 +0100

    staticly assign ssh host keys.

--- scripts/gen_host_vars.py
+++ scripts/gen_host_vars.py
@@ -4,7 +4,10 @@ import os
 import sys
 import uuid
 import base64
+import tempfile
 import subprocess
+
+(_, hostname) = sys.argv
 
 ##########################################################
 # Randomness
@@ -25,7 +28,6 @@ def print_encrypted_var(var_name, content):
                           ) as proc:
         proc.stdin.write(content.encode())
         proc.stdin.close()
-
 
 ##########################################################
 # libvirt data
@@ -48,3 +50,24 @@ print_encrypted_var('fe_sk', fe_sk)
 print_encrypted_var('disk_passphrase', rand_passphrase())
 print_encrypted_var('disk_keyfile', rand_passphrase())
 
+##########################################################
+# SSH keys
+
+with tempfile.TemporaryDirectory() as tempdir:
+    keyfile_path = os.path.join(tempdir, 'ssh_host_ed25519_key')
+    cmd = ['ssh-keygen', '-q', '-t', 'ed25519', '-f', keyfile_path,
+           '-N', '' # no passphrase
+          ]
+    with subprocess.Popen(cmd, stdin=subprocess.PIPE) as proc:
+        proc.stdin.close()
+    with open(keyfile_path) as fd:
+        sk = fd.read()
+    with open(keyfile_path + '.pub') as fd:
+        pk = fd.read()
+
+(pk_type, pk_value, _) = pk.strip().split()
+
+print('ssh_host_keys:')
+print('  ed25519:')
+print('    pk: %s %s root@%s' % (pk_type, pk_value, hostname))
+print_encrypted_var('    sk', sk)
